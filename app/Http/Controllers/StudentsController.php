2223f483a605d273c0431230b1c0219abd74b2d3
Elie Faes <e.faes@weblogin.fr>
2015-06-01 19:56:44 +0200

    Add routes, controller and view

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,124 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Student;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Input;
+
+class StudentsController extends Controller {
+    /**
+     * @var Student
+     */
+    private $student;
+    /**
+     * @var Input
+     */
+    private $input;
+
+    /**
+     * Create a new controller instance.
+     * @param Student $student
+     * @param Input $input
+     */
+    public function __construct(Student $student, Input $input)
+    {
+        $this->middleware('auth');
+        $this->student = $student;
+        $this->input = $input;
+    }
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        $promotions = $this->student
+            ->distinct()
+            ->orderBy('promotion', 'DESC')
+            ->get(['promotion']);
+
+        $activePromotion = $this->input->get('promotion', $promotions->first()->promotion);
+
+        $students = $this->student
+            ->with(['detections' => function($query) {
+                $query->whereBetween('created_at', [Carbon::today(), Carbon::tomorrow()])->orderBy('created_at', 'desc');
+            }])->where('promotion', '=', $activePromotion)
+            ->orderBy('name')
+            ->get();
+
+		return view('students.index')
+            ->with('students', $students)
+            ->with('activePromotion', $activePromotion)
+            ->with('promotions', $promotions->toArray());
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
