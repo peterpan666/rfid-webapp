567e5b933697f8361d8b69f1a7945051c46a1474
Elie Faes <e.faes@weblogin.fr>
2015-06-23 18:39:59 +0200

    Change to sqlite database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,14 +12,14 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        //DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
 		Model::unguard();
 
 		$this->call('UserTableSeeder');
 		$this->call('StudentTableSeeder');
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        //DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 	}
 
 }
